3e88edaf97fe45a340371da1c3ee5feba69f48df
Maxwell Blevins <Maxwell.blevins@geappliances.com>
2025-11-08 14:23:37 -0500

    Hugging face first draft implementation

--- src/app/api/classification/route.jsx
+++ src/app/api/classification/route.jsx
@@ -1,3 +1,38 @@
-export function GET() {
-    return new Response("Make sure people can not just call this route directly.");
+import { pipeline } from '@huggingface/transformers';
+
+let classification = null;
+
+export async function POST(request) {
+    const formData = await request.formData();
+    const image = formData.get('image');
+    
+    if (!image) {
+        return Response.json({
+            success: false,
+            error: 'No image provided'
+        }, { status: 400 });
+    }
+    
+    const arrayBuffer = await image.arrayBuffer();
+    const huggingFaceRequest = Buffer.from(arrayBuffer);
+
+    try {
+        if (!classification) {
+            classification = await pipeline('image-classification', 'watersplash/waste-classification');
+        }
+
+        const response = await classification(huggingFaceRequest);
+
+        return Response.json({
+            success: true,
+            wastePredictions: response
+        });
+
+    } catch (error) {
+        return Response.json({
+            success: false,
+            error: 'Classification failed'
+        }, { status: 500 });
+    }
+
 }
