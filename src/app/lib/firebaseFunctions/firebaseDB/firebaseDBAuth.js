20d47dda240841abe00dc4fd365443223f786903
Ethan Tiller <Ethan.Tiller1@geappliances.com>
2025-11-08 13:06:17 -0500

    push

---
+++ src/app/lib/firebaseFunctions/firebaseDB/firebaseDBAuth.js
@@ -0,0 +1,33 @@
+import { db } from "../../firebase";
+import { doc, setDoc, getDoc } from "firebase/firestore";
+
+export async function createUserProfile(userId, email, firstName, lastName) {
+  // Created at time, items,
+  // Time needs to be in ISO format for consistency
+  const createdAt = new Date().toISOString();
+
+  const userProfile = {
+    email,
+    firstName,
+    lastName,
+    createdAt,
+    lastLogin: createdAt,
+    items: []
+  }
+
+  const userDocRef = doc(db, "users", userId); // Pulls the reference users/{uid}/
+
+  await setDoc(userDocRef, userProfile);
+}
+
+export async function getUserProfile(userId) {
+  //
+  const userDocRef = doc(db, "users", userId);
+  const userDocSnap = await getDoc(userDocRef);
+
+  if (userDocSnap.exists()) {
+    return userDocSnap.data();
+  } else {
+    return null;
+  }
+}
